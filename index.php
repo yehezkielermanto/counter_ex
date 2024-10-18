abd9b35cf9ba8ab8cc1ab810e2ded52ddaee8009
yehezkielermanto <yehezkiel.ermanto28@gmail.com>
2024-10-19 00:12:20 +0700

    update

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ include 'conn/conn.php';
         }
 
         // Run the check on page load
-        setInterval(sendAJAXRequest, 5000);
+        // setInterval(sendAJAXRequest, 1000);
 
         // Optionally, check for changes to localStorage
         // window.addEventListener('storage', checkTrigger);
